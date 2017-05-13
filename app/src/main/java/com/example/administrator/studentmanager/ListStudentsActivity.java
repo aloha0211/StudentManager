74eda6eee3f6c8098c3cdacd1f7277f0e9cb89b5
luonghh <holuong1609@gmail.com>
2017-05-14 00:57:10 +0700

    list students

--- app/src/main/java/com/example/administrator/studentmanager/ListStudentsActivity.java
+++ app/src/main/java/com/example/administrator/studentmanager/ListStudentsActivity.java
@@ -3,11 +3,11 @@ package com.example.administrator.studentmanager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ListStudentsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_list_students);
     }
 }
